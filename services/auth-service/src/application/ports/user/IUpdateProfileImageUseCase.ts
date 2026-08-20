5cf3c0790426e0d8090b667207f63794552fd285
rishadsinu <rishadsinu2233@gmail.com>
2026-08-20 16:57:05 +0530

    feat: add userprof usecase interface

---
+++ services/auth-service/src/application/ports/user/IUpdateProfileImageUseCase.ts
@@ -0,0 +1,5 @@
+import { UpdateProfileImageReqDTO, UpdateProfileImageResponseDTO } from "../../dtos/user/UserProfileImageUploadDTO";
+
+export interface IUpdateProfileImageUseCase {
+    execute(dto: UpdateProfileImageReqDTO): Promise<UpdateProfileImageResponseDTO>;
+}
