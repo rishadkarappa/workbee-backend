a4bf9001ea68596fc37107fe5f102f7b530326c5
rishadsinu <rishadsinu2233@gmail.com>
2026-08-20 14:46:29 +0530

    feat: register Cloudinary service with di

--- services/auth-service/src/infrastructure/di/services.ts
+++ services/auth-service/src/infrastructure/di/services.ts
@@ -10,10 +10,12 @@ import { ITokenService } from "../../domain/services/ITokenService";
 import { IHashService } from "../../domain/services/IHashService";
 import { IEmailService } from "../../domain/services/IEmailService";
 import { IOtpService } from "../../domain/services/IOtpService";
+import { CloudinaryService } from "../services/CloudinaryService";
+import { ICloudinaryService } from "../../domain/services/ICloudinaryService";
 
 //bind sevices as singletons
 container.registerSingleton<IHashService>("HashService", HashService);
 container.registerSingleton<ITokenService>("TokenService", TokenService);
 container.registerSingleton<IEmailService>("EmailService", EmailService);
 container.registerSingleton<IOtpService>("OtpService", OtpService);
-
+container.registerSingleton<ICloudinaryService>("CloudinaryService",CloudinaryService);
--- services/auth-service/src/infrastructure/services/CloudinaryService.ts
+++ services/auth-service/src/infrastructure/services/CloudinaryService.ts
@@ -5,7 +5,7 @@ import cloudinary from "../config/cloudinary.config";
 
 @injectable()
 export class CloudinaryService implements ICloudinaryService {
-    
+
     generateUploadSignature(paramsToSign: Record<string, string | number>): { signature: string; timestamp: number; } {
         const timestamp = Math.round(Date.now() / 1000)
         const signature = cloudinary.utils.api_sign_request({...paramsToSign, timestamp}, ENV.CLOUDINARY_API_SECRET)
@@ -14,7 +14,7 @@ export class CloudinaryService implements ICloudinaryService {
 
     async deleteFile(publicId: string): Promise<void> {
         await cloudinary.uploader.destroy(publicId, {
-            resource_type: ""
+            resource_type: "image"
         })
     }
 }
