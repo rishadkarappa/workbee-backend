84f837ebc162b9961737baf64b33f19eb869b2e1
rishadsinu <rishadsinu2233@gmail.com>
2026-08-21 08:34:05 +0530

    patch: add interface into pay reps in pay-ser on di

--- services/payment-service/src/infrastructure/di/repositories.ts
+++ services/payment-service/src/infrastructure/di/repositories.ts
@@ -1,15 +1,22 @@
 import 'reflect-metadata';
 import { container } from 'tsyringe';
+
+// repository
 import { WalletRepository } from '../database/repositories/WalletRepository';
 import { TransactionRepository } from '../database/repositories/TransactionRepository';
 import { PaymentRepository } from '../database/repositories/PaymentRepository';
 import { PlatformEarningRepository } from '../database/repositories/PlatformEarningRepository';
 
-// ── DI Container registrations
-container.register("WalletRepository", { useClass: WalletRepository });
-container.register("TransactionRepository", { useClass: TransactionRepository });
-container.register("PaymentRepository", { useClass: PaymentRepository });
-container.register("PlatformEarningRepository", { useClass: PlatformEarningRepository });
+// repo interfaces
+import { IWalletRepository } from '../../domain/repositories/IWalletRepository';
+import { ITransactionRepository } from '../../domain/repositories/ITransactionRepository';
+import { IPaymentRepository } from '../../domain/repositories/IPaymentRepository';
+import { IPlatformEarningRepository } from '../../domain/repositories/IPlatformEarningRepository';
 
+// di register
+container.register<IWalletRepository>("WalletRepository", { useClass: WalletRepository });
+container.register<ITransactionRepository>("TransactionRepository", { useClass: TransactionRepository });
+container.register<IPaymentRepository>("PaymentRepository", { useClass: PaymentRepository });
+container.register<IPlatformEarningRepository>("PlatformEarningRepository", { useClass: PlatformEarningRepository });
 
 export { container };
