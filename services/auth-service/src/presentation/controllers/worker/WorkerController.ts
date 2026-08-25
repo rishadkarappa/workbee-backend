8e7a4ad6a7d80d3f34f38241299859f1b0f5cfed
rishadsinu <rishadsinu2233@gmail.com>
2026-08-25 14:03:54 +0530

    feat: add change worker chagne pass controller

--- services/auth-service/src/presentation/controllers/worker/WorkerController.ts
+++ services/auth-service/src/presentation/controllers/worker/WorkerController.ts
@@ -8,11 +8,14 @@ import { IWorkerLoginUseCase } from "../../../application/ports/worker/IWorkerLo
 
 import { IWorkerController } from "../../ports/IWorkerController";
 import { ResponseMessage } from "../../../shared/constants/ResponseMessages";
+import { IChangeWorkerPasswordUseCase } from "../../../application/ports/worker/IChangeWorkerPasswordUseCase";
+import { ErrorMessages } from "../../../shared/constants/ErrorMessages";
 
 @injectable()
 export class WorkerController implements IWorkerController {
     constructor(
         @inject("WorkerLoginUseCase") private readonly _workerLoginUseCase: IWorkerLoginUseCase,
+        @inject("ChangeWorkerPasswordUseCase") private readonly _changeWorkerPasswordUseCase: IChangeWorkerPasswordUseCase,
 
     ) { }
 
@@ -32,9 +35,21 @@ export class WorkerController implements IWorkerController {
 
     async changeWorkerPassword(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
+            const workerId = req.headers["x-user-id"];
             
+            if (!workerId || typeof workerId !== "string") {
+                throw new Error(ErrorMessages.AUTH.UNAUTHORIZED);
+            }
+
+            const { currentPassword, newPassword } = req.body;
+            await this._changeWorkerPasswordUseCase.execute(workerId, { currentPassword, newPassword });
+
+            res
+                .status(HttpStatus.OK)
+                .json(ResponseHelper.success(null, "Password changed successfully"));
+
         } catch (err) {
-            next(err)
+            next(err);
         }
     }
 
