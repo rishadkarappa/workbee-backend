1617fba089b0037cdab16a562a3d3cb10465c83a
rishadsinu <rishadsinu2233@gmail.com>
2026-08-28 14:34:00 +0530

    feat: add admin dash count role and created count i repos

--- services/auth-service/src/domain/repositories/IUserRepository.ts
+++ services/auth-service/src/domain/repositories/IUserRepository.ts
@@ -1,11 +1,15 @@
 import { NewUser, User } from "../entities/User";
 
 export interface IUserRepository {
-    findByEmail(email:string):Promise<User | null>;
-    findById(id:string):Promise<User | null>
+    findByEmail(email: string): Promise<User | null>;
+    findById(id: string): Promise<User | null>
     getUsers(page: number, limit: number, search: string, status?: string): Promise<{ users: User[], total: number }>;
     save(user: User | NewUser): Promise<User>;
     findByIds(ids: string[]): Promise<User[]>;
-    saveNewPassword(userId:string, newHashedPassword:string) : Promise<boolean>;
-    updateProfileImage(userId:string, imageUrl:string, publicId:string):Promise<boolean>;
+    saveNewPassword(userId: string, newHashedPassword: string): Promise<boolean>;
+    updateProfileImage(userId: string, imageUrl: string, publicId: string): Promise<boolean>;
+    
+    //admin dash
+    countByRole(role: string): Promise<number>;
+    countCreatedBetween(role: string, start: Date, end: Date): Promise<number>;
 };
