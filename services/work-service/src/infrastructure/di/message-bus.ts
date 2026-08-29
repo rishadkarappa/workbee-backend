ef29c6f504a10ad67cce941dddfe4075c01ef875
rishadsinu <rishadsinu2233@gmail.com>
2026-08-29 20:19:14 +0530

    add di work prgrs publiser

--- services/work-service/src/infrastructure/di/message-bus.ts
+++ services/work-service/src/infrastructure/di/message-bus.ts
@@ -2,7 +2,11 @@ import { container } from "tsyringe";
 
 import { WorkerEventPublisher } from "../message-bus/WorkerEventPublisher"
 import { WorkerChangePasswordConsumer } from "../message-bus/WorkerChangePasswordConsumer";
+import { WorkProgressEventPublisher } from "../message-bus/WorkProgressEventPublisher";
 
+/** publishers */
 container.registerSingleton("WorkerEventPublisher", WorkerEventPublisher)
+container.registerSingleton("WorkProgressEventPublisher", WorkProgressEventPublisher)
 
+/** consumers */
 container.registerSingleton("WorkerChangePasswordConsumer", WorkerChangePasswordConsumer)
