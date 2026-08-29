e32917d86545507345a2afe42b6dcb2c78694c36
rishadsinu <rishadsinu2233@gmail.com>
2026-08-29 20:17:21 +0530

    feat(wip-notify work progrs to clnt):create work progress publisher

--- services/work-service/src/domain/message-bus/IWorkProgressChangedEvent.ts
+++ services/work-service/src/domain/message-bus/IWorkProgressChangedEvent.ts
@@ -1,7 +1,6 @@
 export type WorkProgress = 'started' | 'ongoing' | 'completed';
 
 /** work progress change to publish event to notify */
-
 export interface IWorkProgressChangedEvent {
     workId:string;
     userId:string;
---
+++ services/work-service/src/infrastructure/message-bus/WorkProgressEventPublisher.ts
@@ -0,0 +1,34 @@
+import { injectable } from "tsyringe";
+import { IWorkProgressChangedEvent } from "../../domain/message-bus/IWorkProgressChangedEvent";
+import { RabbitMQConnection } from "./RabbitMQInitializer";
+import { logger } from "../logger/logger";
+
+@injectable()
+export class WorkProgressEventPublisher {
+    private readonly EXCHANGE = 'workbee.events';
+    async publishWorkProgressChanged(event: IWorkProgressChangedEvent): Promise<void> {
+        try {
+            const channel = await RabbitMQConnection.getChannel()
+            await channel.assertExchange(this.EXCHANGE, "topic", { durable: true })
+
+            const message = Buffer.from(JSON.stringify(event))
+
+            // publishing
+            channel.publish(
+                this.EXCHANGE,
+                'work.progress.changed',
+                message,
+                { persistent: true, contentType: "application/json" }
+            )
+
+            logger.info(`[Work Event Publish] published work.progress.changed ${JSON.stringify(event)}`)
+
+
+
+        } catch (error) {
+            logger.error("[WorkProgressEventPublisher] Failed to publish work progress event:", error);
+
+            throw error;
+        }
+    }
+}
